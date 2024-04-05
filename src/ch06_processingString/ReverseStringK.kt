48333f0d1656f0f5d75360da66618bab47d96345
Wonhui Roh <rwhui012@gmail.com>
2024-04-05 13:57:54 +0900

    [LeetCode] 문자열 뒤집기: Trial 2
    - Trial 1에 대한 코틀린 풀이
    
    - also => 더 우아한 스왑(Swap) in 코틀린
    
    - https://leetcode.com/problems/reverse-string/description/

---
+++ src/ch06_processingString/ReverseStringK.kt
@@ -0,0 +1,21 @@
+package ch06_processingString
+
+/* Trial 2
+- Trial 1에 대한 코틀린 풀이
+
+- also => 더 우아한 스왑(Swap) in 코틀린
+ */
+
+class ReverseStringK {
+  fun reserseString(s: CharArray): Unit {
+    var start = 0
+    var end = s.size - 1;
+    while (start < end) {
+      // also => 더 우아한 스왑(Swap) in 코틀린
+      s[start] = s[end].also { s[end] = s[start] }
+
+      start++
+      end--
+    }
+  }
+}
