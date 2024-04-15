e8208e48b23ac936c1adcbcca3170dcdc6b09c46
Wonhui Roh <rwhui012@gmail.com>
2024-04-15 10:38:26 +0900

    [LeetCode] 두 수의 합: Trial 2
    - 최적화 방법 2: Trial 1 에서 조회 구조 개선
    
    - https://leetcode.com/problems/two-sum/description/

---
+++ src/ch07_array/TwoSumJ_2.java
@@ -0,0 +1,30 @@
+package ch07_array;
+
+/* Trial 2
+
+- 최적화 방법 2: Trial 1 에서 조회 구조 개선
+    - 2개의 for loop -> 하나의 for loop 으로 통합
+    - 시간복잡도: O(n)
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumJ_2 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numsMap = new HashMap<>();
+
+        // 최적화: 2개의 for 문 -> 하나의 for 문으로 통합
+        for (int i = 0; i < nums.length; i++) {
+            int secondNum = target - nums[i];
+            // 맵에 있을 경우 => 정답 리턴
+            if (numsMap.containsKey(secondNum)) {
+                return new int[]{i, numsMap.get(secondNum)};
+            }
+            // 맵에 없을 경우 => 다음번 조회를 위해 맵에 저장
+            numsMap.put(nums[i], i);
+        }
+
+        return null;
+    }
+}
