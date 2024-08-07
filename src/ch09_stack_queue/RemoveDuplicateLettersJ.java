9a15607884a7485451a3bb1ff1558c3078bc0491
Wonhui Roh <rwhui012@gmail.com>
2024-08-07 15:17:17 +0900

    [LeetCode] 중복 문자 제거: Trial 1
    - 재귀 & 집합(Set) 를 이용한 분리
    - "the smallest in lexicographical order"(사전식 순서)
    - String 간의 비교: == vs. equals()
    - TreeSet vs. HashSet
    - 재귀 & replace()
    - 언박싱
    - 본 Trial 1 의 풀이 방법의 실행 속도가 Trial 2 에 비해 훨씬 느린 이유
    - 개선: 익명 클래스 방식 -> 람다 표현식 방식
    
    - https://leetcode.com/problems/remove-duplicate-letters/description/

--- src/ch09_stack_queue/RemoveDuplicateLettersJ.java
+++ src/ch09_stack_queue/RemoveDuplicateLettersJ.java
@@ -18,6 +18,22 @@ package ch09_stack_queue;
 - 언박싱
     - `for (char c : toSortedSet(s))`
     - Character -> char
+- 본 Trial 1 의 풀이 방법의 실행 속도가 Trial 2 에 비해 훨씬 느린 이유
+    - 기본적으로 재귀 풀이의 실행 속도는 좀 더 느린 편
+    - 그렇다고 해도 Trial 1 (151 밀리초) vs. Trial 2 (8 밀리초) -- 이렇게까지 많이 차이가 나는 이유
+        - 문자열을 매번 정렬된 집합으로 만드는 과정에서 각 엘리먼트를 순회해 처리하는 부분에 부담이 큼
+        - 전체 집합과 suffix 집합의 일치 여부를 판단하는 과정도 매우 긴 문자열인 경우 부담이 큼
+- 개선: 익명 클래스 방식 -> 람다 표현식 방식
+    - 익명 클래스 방식 vs. 람다 표현식 방식
+        - 익명 클래스 방식 => 메소드명을 명시함
+        - 람다 표현식 방식
+            - 메소드명 없이 구현함
+            - 훨씬 더 간결한 표현
+            - 더 좋은 성능
+    - 함수형 인터페이스
+        - 단 하나의 메소드만 있는 인터페이스
+            - ex. Comparator, Runnable, Callable 등
+        - 따라서 람다 표현식으로 구현하는 것이 가능함 & 권장됨
  */
 
 import java.util.Set;
@@ -28,18 +44,14 @@ public class RemoveDuplicateLettersJ {
     // String -> 정렬된 Set 로 변환해주는 함수 (직접 구현 필요)
     public Set<Character> toSortedSet(String s) {
         Set<Character> set = new TreeSet<>();
-        /* 비교 기준을 명시적으로 정의할 수도 있을 것
-        Set<Character> set = new TreeSet<>(new Comparator<Character>() {
-            // 재정의한 비교 메서드
-            @Override
-            public int compare(Character o1, Character o2) {
-                if (o1 == o2)
-                    return 0;
-                else if (o1 > o2)
-                    return 1;
-                else
-                    return -1;
-            }
+        /* 비교 기준을 명시적으로 정의할 수도 있을 것 (개선: 익명 클래스 방식 -> 람다 표현식 방식)
+        Set<Character> set = new TreeSet<>((o1, o2) -> {
+            if (o1 == o2)
+                return 0;
+            else if (o1 > o2)
+                return 1;
+            else
+                return -1;
         });
          */
 
