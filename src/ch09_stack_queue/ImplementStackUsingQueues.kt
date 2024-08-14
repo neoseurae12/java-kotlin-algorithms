d9e25929d33965bbe0e722ac2642fbea2ca7c2ab
Wonhui Roh <rwhui012@gmail.com>
2024-08-14 12:13:01 +0900

    [LeetCode] 큐를 이용한 스택 구현: Trial 2
    - Trial 1에 대한 코틀린 풀이
    
    - https://leetcode.com/problems/implement-stack-using-queues/description/

---
+++ src/ch09_stack_queue/ImplementStackUsingQueues.kt
@@ -0,0 +1,30 @@
+package ch09_stack_queue
+
+/* Trial 2
+
+- Trial 1에 대한 코틀린 풀이
+ */
+
+import java.util.*
+
+class ImplementStackUsingQueues {
+  val queue: Queue<Int> = LinkedList()
+
+  fun push(x: Int) {
+    queue.add(x)
+    for (i in 1 until queue.size)
+      queue.add(queue.remove())
+  }
+
+  fun pop(): Int {
+    return queue.remove()
+  }
+
+  fun top(): Int {
+    return queue.peek()
+  }
+
+  fun empty(): Boolean {
+    return queue.isEmpty()
+  }
+}
