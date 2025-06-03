8083f402f5866feff2f4fc61c2fd500f5c616b48
Wonhui Roh <rwhui012@gmail.com>
2025-06-04 00:53:20 +0900

    Trees > DFS > Invert Binary Tree - Recursive ver.
    - 추후 문제분석 정리 예정

--- src/Trees/InvertBinaryTreeRecursive.java
+++ src/Trees/InvertBinaryTreeRecursive.java
@@ -1,20 +1,28 @@
 package Trees;
 
+/*
+- Trees > DFS > Recursive ver.
+ */
+
 import datatype.TreeNode;
 
 public class InvertBinaryTreeRecursive {
-    public TreeNode invertBinaryTreeRecursive(TreeNode root) {
-        // node가 null일 경우 =? 반전(invert)할 것이 없음
-        if (root == null) {
+    public static TreeNode invertBinaryTreeRecursive(TreeNode root) {
+        // Base case: node가 null일 경우 => 반전(invert)할 것이 없음
+        if (root == null)
             return root;
-        }
-        // 현재 노드의 왼쪽과 오른쪽 하위 트리를 바꾸기(swap)
+
+        // 1. process(node)
+        // 현재 노드의 왼쪽과 오른쪽 하위 트리를 반전(invert)시키기
         TreeNode tmp = root.left;
         root.left = root.right;
         root.right = tmp;
-        // 재귀적으로 왼쪽 & 오른쪽 하위 트리를 반전(invert)시키기
+
+        // -> 2. dfs(node.left) -> 3. dfs(node.right)
+        // 왼쪽 & 오른쪽 하위 트리 재귀
         invertBinaryTreeRecursive(root.left);
         invertBinaryTreeRecursive(root.right);
+
         return root;
     }
 }
